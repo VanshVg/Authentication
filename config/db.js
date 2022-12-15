3863ddebbf5c478b109c34981b7cd07cc4b31dda
VanshVg <88078960+VanshVg@users.noreply.github.com>
2022-12-15 22:42:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/authorization";
+const url = "mongodb://localhost:27017/authentication";
 
 module.exports = mongoose.connect(url);
